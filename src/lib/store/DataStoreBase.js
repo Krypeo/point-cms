f849748059cfa75fc0b85c5dbebba350521a2609
Lukas Kaleta <lukas.kaleta@adler.info>
2019-02-07 13:51:37 +0100

    DataStoreBase CRUD

--- src/lib/store/DataStoreBase.js
+++ src/lib/store/DataStoreBase.js
@@ -5,7 +5,7 @@ export class DataStoreBase {
   constructor(api) {
     this.api = api;
   }
-  
+
   @observable _data = [];
   @observable _keys = [];
   @observable loading = false;
@@ -18,11 +18,46 @@ export class DataStoreBase {
     data = await firebase.database().ref(this.api).once('value');
     this._data = data.val();
 
-    keys = Object.keys(toJS(this._data));
+    if (this._data === null) {
+      keys = [];
+    } else {
+      keys = Object.keys(toJS(this._data));
+    }
+
     this._keys = keys;
 
     this.loading = false;
   };
+
+  insert(entity) {
+    return new Promise((resolve, reject) => {
+      firebase.database().ref(this.api).push(entity)
+        .then(() => {
+          resolve();
+        })
+        .catch(reject);
+    })
+  };
+
+  update(id, entity) {
+    return new Promise((resolve, reject) => {
+      firebase.database().ref().child(`${this.api}/${id}`).set(entity)
+        .then(() => {
+          resolve();
+        })
+        .catch(reject);
+    })
+  }
+
+  remove(id) {
+    return new Promise((resolve, reject) => {
+      firebase.database().ref(this.api).child(id).remove()
+        .then(() => {
+          resolve();
+        })
+        .catch(reject);
+    })
+  }
 
   @computed get data() {
     return toJS(this._data);
--- src/pages/management/users/users.component.js
+++ src/pages/management/users/users.component.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import injectSheet from 'react-jss';
 import { observer, inject } from 'mobx-react';
+import { Button } from 'antd';
 
 import style from './users.style';
 import store from './users.store';
@@ -8,14 +9,56 @@ import store from './users.store';
 @inject('settings') @observer
 class Users extends Component {
 
+  remove = async () => {
+    const id = '-LY6xnzlEujbJhTP9EHz';
+    try {
+      await store.remove(id);
+      console.log('Removed');
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  insert = async () => {
+    const entity = {
+      User: 'Pepicek',
+      Name: 'Pepa',
+      Surname: 'Popvsky'
+    }
+    try {
+      await store.insert(entity);
+      console.log('Inserted');
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  update = async () => {
+    const id = '-LY6xnzlEujbJhTP9EHz';
+    const entity = {
+      User: 'Frantise',
+      Name: 'Franta',
+      Surname: 'Frantovsky'
+    }
+    try {
+      await store.update(id, entity);
+      console.log('Inserted');
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
   componentDidMount() {
     store.refresh();
   }
 
   render() {
-    console.log(this.props.settings);
     return (
-      <div onClick={() => this.push()}>Hello Users</div>
+      <div>
+        <Button onClick={() => this.insert()}>Insert</Button>
+        <Button onClick={() => this.update()}>Update</Button>
+        <Button onClick={() => this.delete()}>Delete</Button>
+      </div>
     );
   }
 }
